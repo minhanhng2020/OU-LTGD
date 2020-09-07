5c50e34a9dbaa77e3fd8d3fe1ffaebbfea80ef7d
Nguyen Minh Anh <nma.199x@gmail.com>
2020-09-07 19:38:42 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ASCIIConversion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fSolveQE());
         }
     }
 }
